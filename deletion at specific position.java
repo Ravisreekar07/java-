3dde5e1401a2dd8bb548d1e411dfb3d540dc54d7
Ravisreekar07 <ravisreekaryaleru@gmail.com>
2026-08-18 15:07:13 +0530

    Add files via upload

---
+++ deletion at specific position.java
@@ -0,0 +1,93 @@
+class DoublyLinkedList {
+
+    class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node head = null;
+
+    void insertAtEnd(int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    void deleteAtPosition(int position) {
+
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node temp = head;
+
+        if (position == 1) {
+            head = head.next;
+
+            if (head != null) {
+                head.prev = null;
+            }
+
+            return;
+        }
+
+        for (int i = 1; i < position; i++) {
+            temp = temp.next;
+        }
+
+        if (temp == null) {
+            System.out.println("Invalid position");
+            return;
+        }
+
+        temp.prev.next = temp.next;
+
+        if (temp.next != null) {
+            temp.next.prev = temp.prev;
+        }
+    }
+
+    void display() {
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+
+        // Delete position 3
+        list.deleteAtPosition(3);
+
+        list.display();
+    }
+}
